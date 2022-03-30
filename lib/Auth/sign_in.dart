05f8beca181705432dcaf17ebeceeb5a45da76bb
NIkhilPatel8230 <nikhilkumarbarvadiya@gmail.com>
2022-03-30 17:05:46 +0530

    login done scenner done

--- lib/Auth/sign_in.dart
+++ lib/Auth/sign_in.dart
@@ -22,7 +22,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-final FirebaseAuth _auth = FirebaseAuth.instance;
+final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
 String? name;
@@ -32,13 +32,16 @@ String? imageUrl;
 Future<String?> signInWithGoogle() async {
   await Firebase.initializeApp();
   final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+
   final GoogleSignInAuthentication googleSignInAuthentication =
   await googleSignInAccount!.authentication;
+
   final AuthCredential credential = GoogleAuthProvider.credential(
     accessToken: googleSignInAuthentication.accessToken,
     idToken: googleSignInAuthentication.idToken,
   );
-  final UserCredential authResult = await _auth.signInWithCredential(credential);
+
+  final UserCredential authResult = await firebaseAuth.signInWithCredential(credential);
   final User? user = authResult.user;
   if (user != null) {
     assert(user.email != null);
@@ -52,15 +55,16 @@ Future<String?> signInWithGoogle() async {
     }
     assert(!user.isAnonymous);
     assert(await user.getIdToken() != null);
-    final User? currentUser = _auth.currentUser;
+    final User? currentUser = firebaseAuth.currentUser;
     assert(user.uid == currentUser!.uid);
     print('signInWithGoogle succeeded: $user');
     return '$user';
   }
   return null;
+
 }
 
-Future<void> signOutGoogle() async {
-  await googleSignIn.signOut();
-  print("User Signed Out");
-}
+  Future<void> signOutGoogle() async {
+    await googleSignIn.signOut();
+    print("User Signed Out");
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,8 @@
-// import 'package:abhicreativefashion/Core/Routes/Routes.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:get/get.dart';
-//
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       initialRoute: '/',
-//       getPages: Routes.getAllScreen(),
-//     );
-//   }
-// }
-//
-//
-//
 import 'package:abhicreativefashion/Ui/Home/FirstScreen.dart';
 import 'package:abhicreativefashion/Ui/Home/SecondScreen.dart';
-import 'package:abhicreativefashion/Ui/Screen/HomeScreen.dart';
-import 'package:abhicreativefashion/Ui/Screen/PickupScreen.dart';
-import 'package:abhicreativefashion/Ui/Screen/ProfileScreen.dart';
-import 'package:abhicreativefashion/Ui/Screen/ReturnScreen.dart';
+import 'package:abhicreativefashion/Ui/Login/LoginScreen.dart';
+import 'package:abhicreativefashion/Ui/Login/RegisterScreen.dart';
+import 'package:abhicreativefashion/Ui/Splash/Splash_Screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -41,11 +14,11 @@ void main() async {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        // '/': (context) => Splash_Screen(),
-        // '/LoginScreenRoute': (context) => LoginScreen(),
-        // '/RegisterScreenRoute': (context) => RegisterScreen(),
-        '/': (context) => FirstScreen(),
-        '/SecondScreenRoute': (context) => SecondScreen(),
+        '/': (context) => const Splash_Screen(),
+        '/LoginScreenRoute': (context) => const LoginScreen(),
+        '/RegisterScreenRoute': (context) => const RegisterScreen(),
+        '/FirstScreenRoute': (context) => const FirstScreen(),
+        '/SecondScreenRoute': (context) => const SecondScreen(),
       },
     ),
   );
