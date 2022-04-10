133b35c2d5fe257cd0a3f36a5e0e507b981ceec9
NIkhilPatel8230 <nikhilkumarbarvadiya@gmail.com>
2022-04-10 10:19:59 +0530

    all done

--- lib/Auth/sign_in.dart
+++ lib/Auth/sign_in.dart
@@ -17,55 +17,55 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-import 'package:abhicreativefashion/Data/SharedPefences.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-
-final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn();
-
-String? name;
-String? email;
-String? imageUrl;
-
-Future<String?> signInWithGoogle() async {
-  await Firebase.initializeApp();
-  final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
-
-  final GoogleSignInAuthentication googleSignInAuthentication =
-  await googleSignInAccount!.authentication;
-
-  final AuthCredential credential = GoogleAuthProvider.credential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
-
-  final UserCredential authResult = await firebaseAuth.signInWithCredential(credential);
-  final User? user = authResult.user;
-  if (user != null) {
-    assert(user.email != null);
-    assert(user.displayName != null);
-    assert(user.photoURL != null);
-    name = user.displayName;
-    email = user.email;
-    imageUrl = user.photoURL;
-    if (name!.contains(" ")) {
-      name = name!.substring(0, name!.indexOf(" "));
-    }
-
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-    final User? currentUser = firebaseAuth.currentUser;
-    assert(user.uid == currentUser!.uid);
-    print('signInWithGoogle succeeded: $user');
-    return '$user';
-  }
-  return null;
-
-}
-
-  Future<void> signOutGoogle() async {
-    await googleSignIn.signOut();
-    print("User Signed Out");
-  }
+// import 'package:abhicreativefashion/Data/SharedPefences.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+//
+// final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+// final GoogleSignIn googleSignIn = GoogleSignIn();
+//
+// String? name;
+// String? email;
+// String? imageUrl;
+//
+// Future<String?> signInWithGoogle() async {
+//   await Firebase.initializeApp();
+//   final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+//
+//   final GoogleSignInAuthentication googleSignInAuthentication =
+//   await googleSignInAccount!.authentication;
+//
+//   final AuthCredential credential = GoogleAuthProvider.credential(
+//     accessToken: googleSignInAuthentication.accessToken,
+//     idToken: googleSignInAuthentication.idToken,
+//   );
+//
+//   final UserCredential authResult = await firebaseAuth.signInWithCredential(credential);
+//   final User? user = authResult.user;
+//   if (user != null) {
+//     assert(user.email != null);
+//     assert(user.displayName != null);
+//     assert(user.photoURL != null);
+//     name = user.displayName;
+//     email = user.email;
+//     imageUrl = user.photoURL;
+//     if (name!.contains(" ")) {
+//       name = name!.substring(0, name!.indexOf(" "));
+//     }
+//
+//     assert(!user.isAnonymous);
+//     assert(await user.getIdToken() != null);
+//     final User? currentUser = firebaseAuth.currentUser;
+//     assert(user.uid == currentUser!.uid);
+//     print('signInWithGoogle succeeded: $user');
+//     return '$user';
+//   }
+//   return null;
+//
+// }
+//
+//   Future<void> signOutGoogle() async {
+//     await googleSignIn.signOut();
+//     print("User Signed Out");
+//   }
--- lib/Ui/Screen/HomeScreen.dart
+++ lib/Ui/Screen/HomeScreen.dart
@@ -1,1 +1,1 @@
-import 'package:flutter/material.dart';class Home_Screen extends StatelessWidget {  const Home_Screen({Key? key}) : super(key: key);  @override  Widget build(BuildContext context) {    return Container(      color: Colors.white,    );  }}
+import 'package:flutter/material.dart';class Home_Screen extends StatelessWidget {  const Home_Screen({Key? key}) : super(key: key);  @override  Widget build(BuildContext context) {    return Container(      color: Colors.white,    );  }}
